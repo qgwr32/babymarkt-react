f57c6d428538927dc5569d3e46734ce348b504bd
Jack Diep <jdiep@babymarkt.de>
2022-06-01 11:03:50 +0200

    Prepared use-effect scenario

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,9 +15,9 @@ export default function Header(props) {
   }
 
   return (
-    <div className="bg-white z-10 sticky top-0 border-b border-gray-100">
-      <div className="container grid grid-cols-4 items-center px-8 py-4 mx-auto">
-        <div>Hey {user?.firstName}</div>
+    <div className="sticky top-0 z-10 bg-white border-b border-gray-100">
+      <div className="container grid items-center grid-cols-4 px-8 py-4 mx-auto">
+        <div className="text-xl">Hey {user?.firstName}</div>
         <div className="col-span-2 mx-auto">
           <input
             type="text"
@@ -49,7 +49,7 @@ export default function Header(props) {
 
 function Bubble(props) {
   return (
-    <div className="absolute top-1/2 left-1/2 w-4 h-4 text-xs text-white flex items-center justify-center rounded-full bg-red-600">
+    <div className="absolute flex items-center justify-center w-4 h-4 text-xs text-white bg-red-600 rounded-full top-1/2 left-1/2">
       {props.text}
     </div>
   );
